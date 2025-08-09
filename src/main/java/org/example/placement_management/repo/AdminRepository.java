553b4ca38ba40fa5f42327c5533163e057eb4acf
Guruprasad <guruprasadss2004@gmail.com>
2025-08-09 20:17:14 +0530

    Update AdminRepository.java

--- src/main/java/org/example/placement_management/repo/AdminRepository.java
+++ src/main/java/org/example/placement_management/repo/AdminRepository.java
@@ -1,5 +1,5 @@
 package org.example.placement_management.repo;
-
+//adminRepository
 import org.example.placement_management.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
